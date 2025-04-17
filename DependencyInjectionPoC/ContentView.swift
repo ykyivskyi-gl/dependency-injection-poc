dac461e1c43d1d6952f3328246f545b97d21c085
Yevhen Kyivskyi <yevhen.kyivskyi@glia.com>
2025-04-17 12:23:00 +0200

    Initial Commit

---
+++ DependencyInjectionPoC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DependencyInjectionPoC
+//
+//  Created by Yevhen Kyivskyi on 17/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DependencyInjectionPoC/DependencyInjectionPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionPoCApp.swift
+//  DependencyInjectionPoC
+//
+//  Created by Yevhen Kyivskyi on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
